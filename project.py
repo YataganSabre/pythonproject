5a659d4c8a1bbc623686af1a5fb60adcb8a6eca9
Kaan <71042922+YataganSabre@users.noreply.github.com>
2022-02-09 21:08:36 -0600

    set a not variable instead of using !

--- project.py
+++ project.py
@@ -2,7 +2,7 @@
 import urllib.request
 import os.path
 
-if !(os.path.isfile('awslog.log')):  
+if not(os.path.isfile('awslog.log')):  
     print('You do not have the file, beginning file download with urllib2...')
     url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
     urllib.request.urlretrieve(url, 'awslog.log')
