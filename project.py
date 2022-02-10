55de326f687c52e4935108f9f367ed86d0592142
Kaan <71042922+YataganSabre@users.noreply.github.com>
2022-02-09 20:07:02 -0600

    Update project.py

--- project.py
+++ project.py
@@ -4,4 +4,4 @@ import urllib.request
 print('Beginning file download with urllib2...')
 
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-urllib.request.urlretrieve(url)
+urllib.request.urlretrieve(url, /roor)
