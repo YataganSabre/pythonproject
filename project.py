7c58a291ba778058937ab141c26fa82edca77d47
Kaan <71042922+YataganSabre@users.noreply.github.com>
2022-02-09 20:00:08 -0600

    Trying Urllib

--- project.py
+++ project.py
@@ -1,2 +1,7 @@
 #Created by Kaan M.
-print("Hello World")
+import urllib.request
+
+print('Beginning file download with urllib2...')
+
+url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+urllib.request.urlretrieve(url)
