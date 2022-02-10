89c08fca1b6719583daaf8bb1e0c5eb5807bca15
Kaan <71042922+YataganSabre@users.noreply.github.com>
2022-02-09 21:01:48 -0600

    Lots of stuff
    Tried shot at 6 month counter, changed formatting so that it'll download and keep running file if need be.

--- project.py
+++ project.py
@@ -2,25 +2,27 @@
 import urllib.request
 import os.path
 
-print('Beginning file download with urllib2...')
-
-url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-urllib.request.urlretrieve(url, 'awslog.log')
-
-if os.path.isfile('awslog.log'):
-    # Opening a file
-    file = open("awslog.log","r")
-    Counter = 0
+if !(os.path.isfile('awslog.log')):  
+    print('You do not have the file, beginning file download with urllib2...')
+    url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+    urllib.request.urlretrieve(url, 'awslog.log')
+    print('File downloaded, resuming program.')
+    
+# Opening a file
+file = open("awslog.log","r")
   
-    # Reading from file
-    Content = file.read()
-    CoList = Content.split("\n")
-  
-    for i in CoList:
-        if i:
-            Counter += 1
+# Reading from file
+Content = file.read() 
+# Splitting list by new lines
+CoList = Content.split("\n")
+    
+Counter = 0
+for i in Colist
+    Counter += 1
+    if '30/Apr/1995:23:57:25' in i:
+        print("This is the number of lines from the 24th of October to the 30th of April (6 months): " + Counter)
+Counter = 0
+for i in CoList:
+    Counter += 1
           
-    print("This is the number of lines in the file")
-    print(Counter)
-else:
-    print ("File not exist")
+print("This is the number of lines in the file: " + Counter)
