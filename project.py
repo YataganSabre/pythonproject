978e4952de428df770d3b57ef0e5f9855a78bf09
Kaan <71042922+YataganSabre@users.noreply.github.com>
2022-02-09 20:20:55 -0600

    Added os.path to check if files exist

--- project.py
+++ project.py
@@ -1,7 +1,13 @@
 #Created by Kaan M.
 import urllib.request
+import os.path
 
 print('Beginning file download with urllib2...')
 
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-urllib.request.urlretrieve(url, root)
+urllib.request.urlretrieve(url, 'awslog.log')
+
+if os.path.isfile('awslog.log'):
+    print ("File exist")
+else:
+    print ("File not exist")
