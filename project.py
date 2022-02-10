0ac1c19f69e661be572e34280345c6162b0fe2ec
Kaan <71042922+YataganSabre@users.noreply.github.com>
2022-02-09 20:25:56 -0600

    Check file length counter

--- project.py
+++ project.py
@@ -8,6 +8,19 @@ url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 urllib.request.urlretrieve(url, 'awslog.log')
 
 if os.path.isfile('awslog.log'):
-    print ("File exist")
+    # Opening a file
+    file = open("awslog.log","r")
+    Counter = 0
+  
+    # Reading from file
+    Content = file.read()
+    CoList = Content.split("\n")
+  
+    for i in CoList:
+        if i:
+            Counter += 1
+          
+    print("This is the number of lines in the file")
+    print(Counter)
 else:
     print ("File not exist")
