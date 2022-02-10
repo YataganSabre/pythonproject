168137a3b474429ac47b8615a377b031ba0247fd
Kaan <71042922+YataganSabre@users.noreply.github.com>
2022-02-09 21:20:06 -0600

    Update project.py

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 #Created by Kaan M.
+#This project, per instructions is intended to count the number of requests (lines) for the first 6 months, then the number of lines for the entire log.
 import urllib.request
 import os.path
 
